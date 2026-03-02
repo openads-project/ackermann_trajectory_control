c6ab5c5aba9e4bc6da68ed987adba94309a123df
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-03-02 10:58:53 +0000

    align launch-file to ros2-pkg-create and update author/maintainer

--- launch/ackermann_trajectory_control.launch.py
+++ launch/ackermann_trajectory_control.launch.py
@@ -1,66 +1,57 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import os
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, SetParameter
 from tracetools_launch.action import Trace
 
 def generate_launch_description():
 
-    params_arg = DeclareLaunchArgument('params', default_value='params.yml')
-    config = PathJoinSubstitution([
-        get_package_share_directory("ackermann_trajectory_control"), "config",
-        LaunchConfiguration('params')
-    ])
+    remappable_topics = [
+        DeclareLaunchArgument("ego_data_topic", default_value="~/ego_data"),
+        DeclareLaunchArgument("trajectory_topic", default_value="~/trajectory"),
+        DeclareLaunchArgument("lat_control_active_topic", default_value="~/lat_control_active"),
+        DeclareLaunchArgument("lon_control_active_topic", default_value="~/lon_control_active"),
+        DeclareLaunchArgument("controls_topic", default_value="~/controls"),
+    ]
 
-    name_default = 'ackermann_trajectory_control'
-    name_arg = DeclareLaunchArgument('name', default_value=name_default)
-    trajectory_topic_arg = DeclareLaunchArgument('trajectory_topic', default_value='~/trajectory')
-    ego_data_topic_arg = DeclareLaunchArgument('ego_data_topic', default_value='~/ego_data')
-    controls_topic_arg = DeclareLaunchArgument('controls_topic', default_value='~/controls')
-    lat_control_active_arg = DeclareLaunchArgument('lat_control_active_topic', default_value='~/lat_control_active')
-    lon_control_active_arg = DeclareLaunchArgument('lon_control_active_topic', default_value='~/lon_control_active')
-    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='False')
-    trace_arg = DeclareLaunchArgument('trace', default_value='False', description='Enable tracing')
-    log_level_arg = DeclareLaunchArgument("log_level", default_value="info", description="ROS logging level (debug, info, warn, error, fatal)")
+    args = [
+        DeclareLaunchArgument("name", default_value="ackermann_trajectory_control_node", description="node name"),
+        DeclareLaunchArgument("namespace", default_value="", description="node namespace"),
+        DeclareLaunchArgument("params", default_value=os.path.join(get_package_share_directory("ackermann_trajectory_control"), "config", "params.yml"), description="path to parameter file"),
+        DeclareLaunchArgument("log_level", default_value="info", description="ROS logging level (debug, info, warn, error, fatal)"),
+        DeclareLaunchArgument("startup_state", default_value="None", description="initial lifecycle state"),
+        DeclareLaunchArgument("use_sim_time", default_value="false", description="use simulation clock"),
+        DeclareLaunchArgument("trace", default_value="False", description="Enable tracing"),
+        *remappable_topics,
+    ]
 
-    node = Node(
-        package="ackermann_trajectory_control",
-        executable="ackermann_trajectory_control_node",
-        name=LaunchConfiguration('name'),
-        namespace="",
-        output="screen",
-        emulate_tty=True,
-        arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
-        parameters=[config],
-        remappings=[('~/trajectory', LaunchConfiguration('trajectory_topic')),
-                    ('~/ego_data', LaunchConfiguration('ego_data_topic')),
-                    ('~/controls', LaunchConfiguration('controls_topic')),
-                    ('~/lat_control_active', LaunchConfiguration('lat_control_active_topic')),
-                    ('~/lon_control_active', LaunchConfiguration('lon_control_active_topic'))]
-    )
-
-    trace_action = Trace(
-        session_name='trace',
-        dual_session=True,
-        condition=IfCondition(LaunchConfiguration('trace')),
-    )
+    nodes = [
+        Node(
+            package="ackermann_trajectory_control",
+            executable="ackermann_trajectory_control_node",
+            namespace=LaunchConfiguration("namespace"),
+            name=LaunchConfiguration("name"),
+            parameters=[LaunchConfiguration("params")],
+            arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
+            remappings=[(la.default_value[0].text, LaunchConfiguration(la.name)) for la in remappable_topics],
+            output="screen",
+            emulate_tty=True,
+        ),
+        Trace(
+            session_name='trace',
+            dual_session=True,
+            condition=IfCondition(LaunchConfiguration('trace')),
+        )
+    ]
 
     return LaunchDescription([
-        params_arg,
-        name_arg,
-        trajectory_topic_arg,
-        ego_data_topic_arg,
-        controls_topic_arg,
-        lat_control_active_arg,
-        lon_control_active_arg,
-        use_sim_time_arg,
-        trace_arg,
-        log_level_arg,
-        SetParameter(name='use_sim_time', value=LaunchConfiguration('use_sim_time')),
-        node,
-        trace_action
+        *args,
+        SetParameter("use_sim_time", LaunchConfiguration("use_sim_time")),
+        *nodes,
     ])
