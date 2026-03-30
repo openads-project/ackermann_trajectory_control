5075eec537f432fd9ff922ecd70ac85e6e70cb00
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 16:18:36 +0200

    install ros2_tracing in script

---
+++ docker/before_dependency_installation.sh
@@ -0,0 +1,7 @@
+# remove once [https://github.com/ros2/ros2_tracing/issues/211] is solved in released version
+mkdir -p /tmp/src
+cd /tmp
+git clone --branch jazzy-ika git@github.com:RaphvK/ros2_tracing.git src/ros2_tracing
+rosdep install -i --from-paths src -y
+colcon build --install-base /opt/ros/jazzy/ --merge-install
+rm -r src
