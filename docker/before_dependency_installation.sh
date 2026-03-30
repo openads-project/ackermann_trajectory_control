685220bb1c7431e3fdf59754d5f1413dd2976966
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 16:27:23 +0200

    run rosdep update

--- docker/before_dependency_installation.sh
+++ docker/before_dependency_installation.sh
@@ -2,6 +2,6 @@
 mkdir -p /tmp/src
 cd /tmp
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-rosdep install -i --from-paths src -y
+rosdep update && rosdep install -i --from-paths src -y
 colcon build --install-base /opt/ros/jazzy/ --merge-install
 rm -r src
