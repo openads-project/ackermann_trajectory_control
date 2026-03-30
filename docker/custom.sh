23d8f9e96c5292e2e12b620cc17c1df45852363f
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 15:33:11 +0000

    do not source ros

--- docker/custom.sh
+++ docker/custom.sh
@@ -2,8 +2,7 @@
 # 'message-link instrumentation' and 'dual-session mode' in jazzy
 mkdir -p /tmp/src
 cd /tmp
-source /opt/ros/jazzy/setup.bash
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-apt-get update && rosdep update && rosdep install -i --from-paths src -y
+rosdep install -i --from-paths src -y
 colcon build --install-base /opt/ros/jazzy/ --merge-install --packages-up-to tracetools tracetools_launch
 rm -r src
