a46f847d7f105d35624d8c2d7f62d497d9d95cb8
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 16:35:00 +0200

    test

--- docker/before_dependency_installation.sh
+++ docker/before_dependency_installation.sh
@@ -1,7 +1,8 @@
-# use fork of ros2_tracing to support message-link-instrumentation and dual-session mode in jazzy
+# install fork of ros2_tracing in distro workspace to support
+# 'message-link instrumentation' and 'dual-session mode' in jazzy
 mkdir -p /tmp/src
 cd /tmp
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-rosdep update && rosdep install -i --from-paths src -y
+apt update && rosdep update && rosdep install -i --from-paths src -y
 colcon build --install-base /opt/ros/jazzy/ --merge-install
 rm -r src
