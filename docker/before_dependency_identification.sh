be1143d1ef31fcf8d6a96500353bb1e1e19e73da
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 16:43:59 +0200

    build only required packages

--- docker/before_dependency_identification.sh
+++ docker/before_dependency_identification.sh
@@ -4,6 +4,6 @@ mkdir -p /tmp/src
 cd /tmp
 source /opt/ros/jazzy/setup.bash
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-apt update && rosdep update && rosdep install -i --from-paths src -y
-colcon build --install-base /opt/ros/jazzy/ --merge-install
+apt-get update && rosdep update && rosdep install -i --from-paths src -y
+colcon build --install-base /opt/ros/jazzy/ --merge-install --packages-up-to tracetools tracetools_launch
 rm -r src
