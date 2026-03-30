913b3883b7078f5fc3041c7c0d5ee4bc2f478b8e
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-03-30 16:08:47 +0000

    remove ros2 tracing from .repos and use custom.sh instead

---
+++ docker/custom.sh
@@ -0,0 +1,9 @@
+# overwrite released ros2_tracing packages with fork to support
+# 'message-link instrumentation' and 'dual-session mode' in jazzy
+# remove once [https://github.com/ros2/ros2_tracing/issues/211] is solved in released version
+mkdir -p /tmp/src
+cd /tmp
+git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
+rosdep update && rosdep install -i --from-paths src -y
+colcon build --install-base /opt/ros/jazzy/ --merge-install --packages-up-to tracetools tracetools_launch
+rm -r src
