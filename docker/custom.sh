33a419546f5b8383fbf6c572d29735efa742ed34
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-30 15:38:26 +0000

    test

--- docker/custom.sh
+++ docker/custom.sh
@@ -3,6 +3,6 @@
 mkdir -p /tmp/src
 cd /tmp
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-rosdep install -i --from-paths src -y
+rosdep update && rosdep install -i --from-paths src -y
 colcon build --install-base /opt/ros/jazzy/ --merge-install --packages-up-to tracetools tracetools_launch
 rm -r src
