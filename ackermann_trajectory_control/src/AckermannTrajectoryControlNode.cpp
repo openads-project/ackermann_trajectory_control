cf20c965a95b95b6f3cdb5726f9c9521b4da2bb6
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-04-22 10:24:34 +0000

    add parameter to en/disable odometry when control-cycle runs faster then ego-data is received

--- ackermann_trajectory_control/src/AckermannTrajectoryControlNode.cpp
+++ ackermann_trajectory_control/src/AckermannTrajectoryControlNode.cpp
@@ -54,6 +54,7 @@ AckermannTrajectoryControl::AckermannTrajectoryControl() : Node("ackermann_traje
   this->declareAndLoadParameter("anti_windup_gain", anti_windup_gain_, "Anti-windup back-calculation gain", true, false, false,
                                 0.0, 100.0, 0.1);
   this->declareAndLoadParameter("use_back_calculation", use_back_calculation_, "Enable anti-windup back-calculation", true);
+  this->declareAndLoadParameter("use_odom", use_odom_, "Enable controller-internal odometry integration", true);
   this->declareAndLoadParameter("velocity_lookup", gain_scheduling_velocity_lookup_,
                                 "List of velocities in m/s for which the following gains are defined", false);
   this->declareAndLoadParameter("feed_forward_acceleration_gain", vec_feed_forward_gain_acceleration_,
@@ -177,9 +178,13 @@ rcl_interfaces::msg::SetParametersResult AckermannTrajectoryControl::parametersC
   for (const auto& param : parameters) {
     for (auto& auto_reconfigurable_param : auto_reconfigurable_params_) {
       if (param.get_name() == std::get<0>(auto_reconfigurable_param)) {
+        const bool old_use_odom = use_odom_;
         std::get<1>(auto_reconfigurable_param)(param);
         RCLCPP_INFO(this->get_logger(), "Reconfigured parameter '%s' to: %s", param.get_name().c_str(),
                     param.value_to_string().c_str());
+        if (param.get_name() == "use_odom" && use_odom_ != old_use_odom) {
+          ResetOdometry();
+        }
         break;
       }
     }
@@ -548,14 +553,19 @@ bool AckermannTrajectoryControl::TrjDataProc(const rclcpp::Time& ctrl_time) {
   if (!LinearInterpolation(TIME, THETA, delta_time + lat_t_lookahead_, psi_tgt_)) return false;
   if (!LinearInterpolation(TIME, DELTA, delta_time + lat_t_lookahead_, delta_tgt_)) return false;
 
-  // CalcOdometry
-  double dt_ego = (ctrl_time - cur_vehicle_state_.header.stamp).seconds();
-  double dt_ctrl = (ctrl_time - vhcl_ctrl_output_.header.stamp).seconds();
-  double dt = std::min(dt_ego, dt_ctrl);
-  CalcOdometry(dt);  // Cyclic Control
+  if (use_odom_) {
+    // CalcOdometry
+    double dt_ego = (ctrl_time - cur_vehicle_state_.header.stamp).seconds();
+    double dt_ctrl = (ctrl_time - vhcl_ctrl_output_.header.stamp).seconds();
+    double dt = std::min(dt_ego, dt_ctrl);
+    CalcOdometry(dt);  // Cyclic Control
 
-  dy_ = odom_dy_ - y_tgt_;
-  dpsi_ = odom_dpsi_ - psi_tgt_;
+    dy_ = odom_dy_ - y_tgt_;
+    dpsi_ = odom_dpsi_ - psi_tgt_;
+  } else {
+    dy_ = -y_tgt_;
+    dpsi_ = -psi_tgt_;
+  }
   return true;
 }
 
